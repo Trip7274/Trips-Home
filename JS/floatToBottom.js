77fdce733f56aea6e9862bba1885bf27d142ad97
Trip7274 <github@trps.dev>
2026-02-13 22:55:00 +0300

    Update some content and styling

--- JS/floatToBottom.js
+++ JS/floatToBottom.js
@@ -1,9 +1,16 @@
-function floatToBottom() {
-	const contentHeight = document.getElementById("MainDiv").clientHeight;
-	const headerHeight = document.querySelector('header').clientHeight;
+const contentHeight = document.getElementById("MainDiv").clientHeight,
+	headerHeight = document.querySelector('header').clientHeight,
+	delay = 250;
 
+let timeout;
+
+function floatToBottom() {
 	// The math is for the margin of #MasterDiv
 	document.getElementById("MasterGrid").style.gridTemplateRows = `${Math.max(window.innerHeight - contentHeight - (14 + (window.innerWidth * 0.03621)), headerHeight)}px 1fr`;
 }
 
 window.addEventListener("load", floatToBottom, false);
+window.addEventListener("resize", function () {
+	clearTimeout(timeout)
+	timeout = setTimeout(floatToBottom, delay)
+});
