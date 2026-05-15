949a775696e09407460ff116cf3f4a82d30be883
Trip7274 <github@trps.dev>
2026-05-15 07:43:58 +0300

    Update buildSass.sh to check for Sass instead of just the env var

--- buildSass.sh
+++ buildSass.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 
-# This script is intended to run on Cloudflare Pages, and for local file watchers, if running with a pre-installed sass, set the enviroment variable USE_SYSTEM_SASS to 1
-
-if [ ! -f SassDir/dart-sass/dart-sass/sass ] && [ "$USE_SYSTEM_SASS" != "1" ]; then
+if ! sass --version > /dev/null; then
 	if [ ! -f SassDir ]; then
 	    mkdir "SassDir"
 	fi
     echo "Downloading SassDir..."
 
-    if ! curl -sL 'https://github.com/sass/dart-sass/releases/download/1.85.1/dart-sass-1.85.1-linux-x64.tar.gz' > SassDir/dart-sass-1.85.1-linux-x64.tar.gz; then
+    if ! curl -sL 'https://github.com/sass/dart-sass/releases/download/1.99.0/dart-sass-1.99.0-linux-x64.tar.gz' > SassDir/dart-sass.tar.gz; then
         echo "Error while downloading Sass! Code: $?"
         exit
     fi
@@ -20,12 +18,12 @@ if [ ! -f SassDir/dart-sass/dart-sass/sass ] && [ "$USE_SYSTEM_SASS" != "1" ]; t
 	    mkdir "SassDir/dart-sass"
 	fi
 
-    if ! tar -C 'SassDir/dart-sass' -xzf 'SassDir/dart-sass-1.85.1-linux-x64.tar.gz'; then
+    if ! tar -C 'SassDir/dart-sass' -xzf 'SassDir/dart-sass.tar.gz'; then
         echo "Error while extracting Sass! Code: $?"
         exit
     fi
 
-    rm SassDir/dart-sass-1.85.1-linux-x64.tar.gz
+    rm SassDir/dart-sass.tar.gz
     echo "Sass extracted! ($?)"
     export PATH="SassDir/dart-sass/dart-sass:$PATH"
 fi
